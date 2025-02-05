eb21e9a60d6f730f26c54b6124135a445e5d8c28
NguyenQui9112003 <147489314+NguyenQui9112003@users.noreply.github.com>
2025-02-05 21:31:30 +0700

    add home and login page

--- sec-fe-shopee/src/main.tsx
+++ sec-fe-shopee/src/main.tsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+
+import { LoginPage } from './page/login.tsx';
+import { HomePage } from './page/home.tsx';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Navigate to="/home" replace />,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/home",
+      element: <HomePage />,
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+    },
+  }
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
---
+++ sec-fe-shopee/src/page/home.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/common/header";
+
+export const HomePage = () => {
+    return (
+        <>
+            <Header></Header>
+            Home Page
+        </>
+    );
+}
